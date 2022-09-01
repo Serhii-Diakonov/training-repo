9cf456ce2177931416c70be0df962426900ae19e
Sergey-Dyakonov <sergeidyakonov222@gmail.com>
2022-09-02 02:58:53 +0300

    Task "Chars in string" done

--- src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
@@ -1,10 +1,20 @@
 package com.knubisoft.tasks.algorithm.charsinstring;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CharsInStringImpl implements CharsInString {
     @Override
     public Map<Character, Integer> charactersCount(String text) {
-        return null;
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (char c : text.toCharArray()) {
+            if (c != ' ') {
+                map.merge(c, 1, Integer::sum);
+                /*if (map.get(c) == null) {
+                map.put(c, 1);
+                } else map.put(c, map.get(c) + 1);*/
+            }
+        }
+        return map;
     }
 }
