80545ef281aaa67cbdcf3f9351f5e1610a1788d2
Sergey-Dyakonov <sergeidyakonov222@gmail.com>
2022-09-02 03:25:06 +0300

    Task "LuckyTicket" done

--- src/main/java/com/knubisoft/tasks/algorithm/fibonacci/FibonacciImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/fibonacci/FibonacciImpl.java
@@ -3,6 +3,15 @@ package com.knubisoft.tasks.algorithm.fibonacci;
 public class FibonacciImpl implements Fibonacci {
     @Override
     public int generateNFibonacciSequence(int n) {
-        return -1;
+        if (n < 2) {
+            return 0;
+        }
+        int firstVal = 1, secondVal = 2, sum = 3;
+        for (int i = 2; i < n; i++){
+            sum = firstVal + secondVal;
+            firstVal = secondVal;
+            secondVal = sum;
+        }
+        return sum;
     }
 }
--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -1,8 +1,12 @@
 package com.knubisoft.tasks.algorithm.luckyticket;
 
+import java.util.regex.Pattern;
+
 public class LuckyTicketImpl implements LuckyTicket {
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        return false;
+        return ticket != null &&
+                Pattern.matches("\\d{6,8}", ticket) &&
+                new StringBuilder(ticket).reverse().toString().equals(ticket);
     }
 }
